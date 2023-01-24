ca7c3ba7fcf52ffd5c744164f56dad32c9a3db6f
Anthony Keith <jt1keith@gmail.com>
2023-01-24 14:21:49 -0800

    fixed typos and grammar

--- js/app.js
+++ js/app.js
@@ -12,21 +12,21 @@ if (guacGuess === 'yes' || guacGuess === 'y') {
   correctAnswers += 1;
   console.log(correctAnswers);
 } else if (guacGuess === 'no' || guacGuess === 'n'){
-  alert("Sorry! I do like guac!")
+  alert("Sorry! I do like guac!");
 } else {
-  alert("Please choose yes or no!")
+  alert("Please choose yes or no!");
 }
-console.log(correctAnswers);
+//console.log(correctAnswers);
 
 let gamerGuess = prompt("Do I like to play videogames?").toLowerCase()
 //console.log(gamerGuess);
 if (gamerGuess === 'yes' || gamerGuess === 'y') {
-  alert("Correct, I like to play videogames!")
+  alert("Correct, I like to play videogames!");
   correctAnswers += 1;
 } else if (gamerGuess === 'no' || gamerGuess === 'n') {
-  alert("Sorry! I really enjoy playing video games")
+  alert("Sorry! I really enjoy playing video games");
 } else {
-  alert("Please choose yes or no!")
+  alert("Please choose yes or no!");
 }
 
 //console.log(correctAnswers);
@@ -34,12 +34,12 @@ if (gamerGuess === 'yes' || gamerGuess === 'y') {
 let catGuess = prompt("Do I own a cat").toLowerCase()
 //console.log(catGuess);
 if (catGuess === 'yes' || catGuess === 'y') {
-  alert("Correct, his name is Winston!")
+  alert("Correct, his name is Winston!");
   correctAnswers += 1;
 } else if (catGuess === 'no' || catGuess === 'n'){
-  alert("Sorry! my cat is named Winston!")
+  alert("Sorry! my cat is named Winston!");
 } else {
-  alert("Please choose yes or no!")
+  alert("Please choose yes or no!");
 }
 
 let washingtonGuess = prompt("Am I from Washington?").toLowerCase()
@@ -48,26 +48,26 @@ if (washingtonGuess === 'yes' || washingtonGuess === 'y') {
   alert("Correct, I am from Bremerton, WA");
   correctAnswers += 1;
 } else if (washingtonGuess === 'no' || washingtonGuess === 'n'){
-  alert("Incorrect, I am from Bremerton, WA")
+  alert("Incorrect, I am from Bremerton, WA");
 } else {
-  alert("Please choose yes or no!")
+  alert("Please choose yes or no!");
 }
 
 let animeGuess = prompt("Do I like anime?").toLowerCase()
 //console.log(animeGuess);
 if (animeGuess === 'yes' || animeGuess === 'y') {
-  alert("Correct, I like anime!")
+  alert("Correct, I like anime!");
   correctAnswers += 1;
 } else if (animeGuess === 'no' || animeGuess === 'n'){
   alert("Sorry! I'm a huge fan!");
 } else {
-  alert("Please choose yes or no!")
+  alert("Please choose yes or no!");
 }
 
 if (correctAnswers === 1) {
   alert("Congratulations " + username + ", you got 1 correct!")
 } else if (correctAnswers === 0) {
-  alert("Sorry, " + username + " you got none right :(")
+  alert("Sorry, " + username + " you got none right :(");
 } else {
-  alert("Congratulations " + username + ", you got " + correctAnswers + " correct answers!")
+  alert("Congratulations " + username + ", you got " + correctAnswers + " correct answers!");
 }
