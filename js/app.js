3ca6e7aa78f64efa89d6c88832b6c4e57831121e
Phil Chaplin <pchapl1@gmail.com>
2023-01-26 11:25:01 -0800

    moved gamer question into a function

--- js/app.js
+++ js/app.js
@@ -24,18 +24,19 @@ const guacQuestion = () => {
   //console.log(correctAnswers);
 }
 
-let gamerGuess = prompt('Do I like to play videogames?').toLowerCase();
-//console.log(gamerGuess);
-if (gamerGuess === 'yes' || gamerGuess === 'y') {
-  alert('Correct, I like to play videogames!');
-  correctAnswers += 1;
-} else if (gamerGuess === 'no' || gamerGuess === 'n') {
-  alert('Sorry! I really enjoy playing video games');
-} else {
-  alert('Please choose yes or no!');
-}
-
+const gamerQuestion = ()=> {
+  let gamerGuess = prompt('Do I like to play videogames?').toLowerCase();
+  //console.log(gamerGuess);
+  if (gamerGuess === 'yes' || gamerGuess === 'y') {
+    alert('Correct, I like to play videogames!');
+    correctAnswers += 1;
+  } else if (gamerGuess === 'no' || gamerGuess === 'n') {
+    alert('Sorry! I really enjoy playing video games');
+  } else {
+    alert('Please choose yes or no!');
+  }
 //console.log(correctAnswers);
+}
 
 let catGuess = prompt('Do I own a cat').toLowerCase();
 //console.log(catGuess);
